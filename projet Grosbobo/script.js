7ef5ab1b20fd40972db63a519ae6266bfce0f6fd
greg13170 <39373118+greg13170@users.noreply.github.com>
2018-06-05 18:06:13 +0200

    Add files via upload

---
+++ projet Grosbobo/script.js
@@ -0,0 +1,48 @@
+
+ // le clik affiche
+ 
+ 
+    function epaule() {
+    document.getElementById('Epaule').style.visibility = "visible";
+	}
+	
+	function coude() {
+    document.getElementById('Coude').style.visibility = "visible";
+	}
+	function poignet() {
+    document.getElementById('Poignet').style.visibility = "visible";
+	}
+	function hanche() {
+    document.getElementById('Hanche').style.visibility = "visible";
+	}
+	function genou() {
+    document.getElementById('Genou').style.visibility = "visible";
+	}
+	function cheville() {
+    document.getElementById('Cheville').style.visibility = "visible";
+	}
+	
+	
+
+// quand le curseur n'est plus sur le bouton
+
+ 
+    function out1() {
+	document.getElementById('Epaule').style.visibility = "hidden";
+	}
+	function out2() {
+	document.getElementById('Coude').style.visibility = "hidden";
+	}
+	function out3() {
+	document.getElementById('Poignet').style.visibility = "hidden";
+	}
+	function out4() {
+	document.getElementById('Hanche').style.visibility = "hidden";
+	}
+	function out5() {
+	document.getElementById('Genou').style.visibility = "hidden";
+	}
+	function out6() {
+	document.getElementById('Cheville').style.visibility = "hidden";
+	}
+	
